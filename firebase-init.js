d91c852cfa01fc07fc3995c7f446efce5488282a
kar the dev <122060951+karterman20@users.noreply.github.com>
2023-11-28 16:44:54 -0600

    Create firebase-init.js

---
+++ firebase-init.js
@@ -0,0 +1,6 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js'
+import { getAnalytics } from 'https://www.gstatic.com/firebasejs/10.6.0/firebase-analytics.js'
+import { getAuth } from 'https://www.gstatic.com/firebasejs/10.6.0/firebase-auth.js'
+import { getFirestore, collection, getDocs } from 'https://www.gstatic.com/firebasejs/10.6.0/firebase-firestore.js'
+
+export {initializeApp, getAnalytics, getAuth, getFirestore, getDocs, collection}
