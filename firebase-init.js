281b2892dad10fa3ac370e29346e559928f27103
Eternal Flame Studios <eternalflamegamesofficial@outlook.com>
2023-12-01 17:10:50 -0600

    Delete firebase-init.js

--- firebase-init.js
+++
@@ -1,6 +0,0 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js'
-import { getAnalytics } from 'https://www.gstatic.com/firebasejs/10.6.0/firebase-analytics.js'
-import { getAuth } from 'https://www.gstatic.com/firebasejs/10.6.0/firebase-auth.js'
-import { getFirestore, collection, getDocs } from 'https://www.gstatic.com/firebasejs/10.6.0/firebase-firestore.js'
-
-export {initializeApp, getAnalytics, getAuth, getFirestore, getDocs, collection}
