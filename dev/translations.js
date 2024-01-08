737a6119569185bb8c94b1a10663c2e3d8c7b9f9
kar the dev <122060951+kar-the-dev@users.noreply.github.com>
2024-01-08 17:22:49 -0600

    Create translations.js

---
+++ dev/translations.js
@@ -0,0 +1,96 @@
+while(translated.includes("a")){
+  translated = translated.replace(" @");
+}
+while(translated.includes(" hello")){
+  translated = translated.replace(" hello", " hey");
+}
+while(translated.includes(" hello")){
+  translated = translated.replace(" hello", " hey");
+}
+while(translated.includes(" hello")){
+  translated = translated.replace(" hello", " hey");
+}
+while(translated.includes(" hello")){
+  translated = translated.replace(" hello", " hey");
+}
+while(translated.includes(" hello")){
+  translated = translated.replace(" hello", " hey");
+}
+while(translated.includes(" hello")){
+  translated = translated.replace(" hello", " hey");
+}
+while(translated.includes(" hello")){
+  translated = translated.replace(" hello", " hey");
+}
+while(translated.includes(" hello")){
+  translated = translated.replace(" hello", " hey");
+}
+while(translated.includes(" hello")){
+  translated = translated.replace(" hello", " hey");
+}
+while(translated.includes(" hello")){
+  translated = translated.replace(" hello", " hey");
+}
+while(translated.includes(" hello")){
+  translated = translated.replace(" hello", " hey");
+}
+while(translated.includes(" hello")){
+  translated = translated.replace(" hello", " hey");
+}
+while(translated.includes(" hello")){
+  translated = translated.replace(" hello", " hey");
+}
+while(translated.includes(" hello")){
+  translated = translated.replace(" hello", " hey");
+}
+while(translated.includes(" hello")){
+  translated = translated.replace(" hello", " hey");
+}
+while(translated.includes(" hello")){
+  translated = translated.replace(" hello", " hey");
+}
+while(translated.includes(" hello")){
+  translated = translated.replace(" hello", " hey");
+}
+while(translated.includes(" hello")){
+  translated = translated.replace(" hello", " hey");
+}
+while(translated.includes(" hello")){
+  translated = translated.replace(" hello", " hey");
+}
+while(translated.includes(" hello")){
+  translated = translated.replace(" hello", " hey");
+}
+while(translated.includes(" hello")){
+  translated = translated.replace(" hello", " hey");
+}
+while(translated.includes(" hello")){
+  translated = translated.replace(" hello", " hey");
+}
+while(translated.includes(" hello")){
+  translated = translated.replace(" hello", " hey");
+}
+while(translated.includes(" hello")){
+  translated = translated.replace(" hello", " hey");
+}
+while(translated.includes(" hello")){
+  translated = translated.replace(" hello", " hey");
+}
+while(translated.includes(" hello")){
+  translated = translated.replace(" hello", " hey");
+}
+while(translated.includes(" hello")){
+  translated = translated.replace(" hello", " hey");
+}
+while(translated.includes(" hello")){
+  translated = translated.replace(" hello", " hey");
+}
+while(translated.includes(" hello")){
+  translated = translated.replace(" hello", " hey");
+}
+while(translated.includes(" hello")){
+  translated = translated.replace(" hello", " hey");
+}
+while(translated.includes(" hello")){
+  translated = translated.replace(" hello", " hey");
+}
