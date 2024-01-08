d6a977dcb21fe5cf6f8b816e459897788ff751c2
kar the dev <122060951+kar-the-dev@users.noreply.github.com>
2024-01-08 17:34:34 -0600

    Delete dev/script.js

--- dev/script.js
+++
@@ -1,6 +0,0 @@
-var translated;
-document.getElementById('input').onkeyup = function(e){
-  translated = " " + document.getElementById('input').value;
-  translate(e);
-  document.getElementById('output').innerHTML = translated;
-}
